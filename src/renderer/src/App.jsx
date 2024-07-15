d46841061e957c532a3220b26c39979809bb4a38
Baek <Sahwall@cpst.co.kr>
2024-07-15 09:32:59 +0900

    Initialize

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <div className="action">
+        <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+          Send IPC
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
